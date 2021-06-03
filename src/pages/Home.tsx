cd8ab50103758b7485ddf214296342c9817b6e7f
Albert Dias <albert.uft@gmail.com>
2021-06-03 01:33:58 -0300

    finish challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,32 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if(newTaskTitle !== ''){
+      const data = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      }
+      setTasks(oldState => [...oldState, data]);
+    }
+    
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const updateTask = tasks.map(task => {
+      return (task.id === id)?{...task, done: !task.done}:task;
+    } );
+
+    setTasks(updateTask)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldTasks => oldTasks.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
